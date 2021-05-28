60cba6f898b1e6e650b50e5c9a1d753988325234
Pawanlal <pawanlalganesh@gmail.com>
2021-05-28 22:43:16 +0545

    [Solved] file update issue

--- websites/models.py
+++ websites/models.py
@@ -14,7 +14,7 @@ class Website(models.Model):
     def __str__(self):
         return str(self.url)
 
-    def save(self, *args, **kwargs):
+    def qr_code_generator(self):
         qrcode_img = qrcode.make(self.url)
         canvas = Image.new('RGB', (370, 370), 'white')
         canvas.paste(qrcode_img)
@@ -23,4 +23,22 @@ class Website(models.Model):
         canvas.save(buffer, 'PNG')
         self.qr_code.save(fname, File(buffer), save=False)
         canvas.close()
+
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        instance = super().from_db(db, field_names, values)
+        instance._loaded_values = dict(zip(field_names, values))
+
+        # instance = super().from_db(db, field_names, values)
+        # values = dict(zip(field_names, values))
+        # instance._prev_url = values.get('url')
+
+        return instance
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.qr_code_generator()
+        if self.id and self.url != self._loaded_values['url']:
+            self.qr_code.delete(save=False)
+            self.qr_code_generator()
         super().save(*args, **kwargs)
